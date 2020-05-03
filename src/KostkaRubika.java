f248fa35a9f64f11d5e10894496f34479f074900
maciejkaczka <51151069+maciejkaczka@users.noreply.github.com>
2020-05-03 17:44:52 +0200

    Wywalone śmieci tekstowe i kamera po środku

--- src/KostkaRubika.java
+++ src/KostkaRubika.java
@@ -51,33 +51,19 @@ public class KostkaRubika extends JFrame {
         objRotate.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
         int i=-1,j=-1,k=-1;
 
-        for(float x=0;x<0.93;x=x+(float)0.31)
+        for(float x=(float)-0.31;x<0.32;x=x+(float)0.31)
         {
             j=-1;
             i++;
-            System.out.println("i :" + i);
-            for(float y=0;y<0.93;y=y+(float)0.31)
+            for(float y=(float)-0.31;y<0.32;y=y+(float)0.31)
             {
                 k=-1;
                 j++;
-                System.out.println("j :" + j);
-                for(float z=0;z<0.93;z=z+(float)0.31)
+                for(float z=(float)-0.31;z<0.32;z=z+(float)0.31)
                 {
                     k++;
-                    System.out.println("k :" + k);
                     Cubie kostka = new Cubie(size,x,y,z);
                     kubiki[i][j][k] = kostka;
-
-
-                    //jebanie sie z tekstem---------------------------------
-                    Font3D font = new Font3D(new Font("Arial", Font.PLAIN, 1), new FontExtrusion());
-                    Text3D tekst = new Text3D(font,"indeks" + i + j +k);
-                    Point3f punkt =  new Point3f(i,j,k);
-                    tekst.setPosition(punkt);
-                    Shape3D textShape = new Shape3D();
-                    textShape.setGeometry(tekst);
-                    objRotate.addChild(textShape);
-                    //--------------------------------------------
 
                     objRotate.addChild(kostka.getTg());
                 }
@@ -91,8 +77,7 @@ public class KostkaRubika extends JFrame {
 
         MouseRotate myMouseRotate = new MouseRotate();
         myMouseRotate.setTransformGroup(objRotate);
-        Point3d punktObrotu = new Point3d(0.48,0.48,0.48);
-        myMouseRotate.setSchedulingBounds(new BoundingSphere( punktObrotu,0.48));
+        myMouseRotate.setSchedulingBounds(new BoundingSphere());
         objRoot.addChild(myMouseRotate);
 
 
